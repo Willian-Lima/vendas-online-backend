faefe405d799f93ee7c829898cc8a465583efe9b
Willian Lima <willianbatistalima1.0@gmail.com>
2023-07-04 09:22:11 -0300

    feat: create function createCategory

--- src/category/__tests__/category.service.spec.ts
+++ src/category/__tests__/category.service.spec.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CategoryEntity } from '../entities/category.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { categoryMock } from '../__mocks__/category.mock';
+import { createCategoryMock } from '../__mocks__/create-category.mock';
 
 describe('CategoryService', () => {
   let service: CategoryService;
@@ -16,8 +17,9 @@ describe('CategoryService', () => {
         {
           provide: getRepositoryToken(CategoryEntity),
           useValue: {
+            findOne: jest.fn().mockResolvedValue(categoryMock),
             find: jest.fn().mockResolvedValue([categoryMock]),
-            save: jest.fn().mockResolvedValue([categoryMock]),
+            save: jest.fn().mockResolvedValue(categoryMock),
           },
         },
       ],
@@ -48,5 +50,23 @@ describe('CategoryService', () => {
     jest.spyOn(categoryRepository, 'find').mockRejectedValue(new Error());
 
     expect(service.findAllCategories()).rejects.toThrowError();
+  });
+
+  it('should return error if exist category name', async () => {
+    expect(service.createCategory(createCategoryMock)).rejects.toThrowError();
+  });
+
+  it('should return category after save', async () => {
+    jest.spyOn(categoryRepository, 'findOne').mockResolvedValue(undefined);
+
+    const category = await service.createCategory(createCategoryMock);
+
+    expect(category).toEqual(categoryMock);
+  });
+
+  it('should return error if caregory in find by name empty', async () => {
+    jest.spyOn(categoryRepository, 'findOne').mockRejectedValue(new Error());
+
+    expect(service.findCategoryByName(categoryMock.name)).rejects.toThrowError();
   });
 });
--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ReturnCategory } from './dtos/return-category.dto';
 import { CategoryService } from './category.service';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
+import { Validate } from 'class-validator';
+import { CategoryEntity } from './entities/category.entity';
+import { createCategory } from './dtos/create-category.dto';
 
 @Roles(UserType.Admin, UserType.User)
 @Controller('category')
@@ -14,5 +17,12 @@ export class CategoryController {
     return (await this.categoryService.findAllCategories()).map(
       (category) => new ReturnCategory(category)
     );
+  }
+
+  @Roles(UserType.Admin, UserType.User)
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createCategory(@Body() createCategory: createCategory): Promise<CategoryEntity> {
+    return this.categoryService.createCategory(createCategory);
   }
 }
