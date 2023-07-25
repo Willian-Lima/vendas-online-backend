9ce0f82c91ded0d4b39a2f8722f82a2570b2cb55
Willian Lima <willianbatistalima1.0@gmail.com>
2023-07-25 09:17:50 -0300

    feat: create cart of user

--- src/decorators/user-id.decorator.ts
+++ src/decorators/user-id.decorator.ts
@@ -6,7 +6,7 @@ export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
 
   const loginPayload = authorizationToLoginPayload(authorization);
 
-  console.log('authorization', authorization);
+  //console.log('authorization', authorization);
 
   return loginPayload?.id;
 });
