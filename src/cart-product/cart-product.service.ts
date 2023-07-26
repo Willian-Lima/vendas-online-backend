e371860c437bcc8e92ce582837068c284bf7b4d1
Willian Lima <willianbatistalima1.0@gmail.com>
2023-07-26 11:46:35 -0300

    feat: add product existence check in cartProductService

--- src/cart-product/cart-product.service.ts
+++ src/cart-product/cart-product.service.ts
@@ -4,12 +4,14 @@ import { CartProductEntity } from './entities/cart-product.entity';
 import { Repository } from 'typeorm';
 import { InsertCartDTO } from 'src/cart/dtos/insert-cart.dto';
 import { CartEntity } from 'src/cart/entities/cart.entity';
+import { ProductService } from 'src/product/product.service';
 
 @Injectable()
 export class CartProductService {
   constructor(
     @InjectRepository(CartProductEntity)
-    private readonly cartProductRepository: Repository<CartProductEntity>
+    private readonly cartProductRepository: Repository<CartProductEntity>,
+    private readonly productService: ProductService
   ) {}
 
   async verifyProductInCart(productId: number, cartId: number): Promise<CartProductEntity> {
@@ -42,6 +44,8 @@ export class CartProductService {
     insertCartDTO: InsertCartDTO,
     cart: CartEntity
   ): Promise<CartProductEntity> {
+    await this.productService.findProductById(insertCartDTO.productId);
+
     const cartProduct = await this.verifyProductInCart(insertCartDTO.productId, cart.id).catch(
       () => undefined
     );
